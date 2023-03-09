78ce411d1898165a82f38a44b601b2839fc690c3
Paul KPADONOU <pmkpaul87@gmail.com>
2023-03-09 12:22:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hello-city
+
+Ceci est une application laravel qui affiche l'heure courante dans 
+votr ville de résidence
---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello city</title>
+    </head>
+    <body>
+       <h1>Hello From Cotonou!</h1>
+
+       <p>It's currently {{ date('H:i')}} </p>
+
+       <footer>
+        <p>&copy; Copyright {{ date('Y')}} &middot; <a href="#">About us</a></p>
+       </footer>
+    </body>
+</html>
